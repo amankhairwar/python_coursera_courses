e4c0de222da58f7757d76d2c17f5f0308399afb3
amankhairwar <56715191+amankhairwar@users.noreply.github.com>
2020-04-24 19:26:51 +0530

    Create data_dict.py

---
+++ data_dict.py
@@ -0,0 +1,21 @@
+
+demo={'name':'aman',
+     'marks': 100,
+     'age': 21  
+     }
+print(demo)
+print(type('name'))
+print(type('aman'))
+
+cipher = {'p': 'o', 'y': 'h', 't': 'n',
+          'h': 't', 'o': 'y', 'n': 'p'} 
+print(cipher)
+goodinstructors = {'Rixner': True, 'Warren': False}
+print(goodinstructors)
+
+cities = {'China': ['Shanghai', 'Beijing'],
+          'USA': ['New York', 'Los Angeles'],
+          'Spain': ['Madrid', 'Barcelona'],
+          'Australia': ['Sydney', 'Melbourne'],
+          'Texas': ['Houston', 'San Antonio']}
+print(cities)
